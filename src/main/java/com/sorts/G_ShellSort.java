2287c645c4b4d59adae7404e98bbbb513e75e837
huaaijia <huaaijia@letv.com>
2017-02-13 18:38:16 +0800

    ShellSort

--- src/main/java/com/sorts/G_ShellSort.java
+++ src/main/java/com/sorts/G_ShellSort.java
@@ -14,8 +14,29 @@ public class G_ShellSort {
         print("End:", data);
     }
 
+    /**
+     * 缩小步长排序
+     * @param data
+     */
     public static void shellSort(int[] data) {
+        for(int step = data.length/2; step>=1; step = step/2){//步长
+            for(int i=0; i<step; i++){//当前步长下，分多少组排序
 
+                //改良版快速排序
+                for (int j = i+step; j < data.length; j=j+step) {
+                    if (data[j] < data[j - step]) {
+                        int temp = data[j];
+                        int j2 = j - step;
+                        while (j2 >= 0 && temp < data[j2]) {
+                            data[j2 + step] = data[j2];
+                            j2=j2-step;
+                        }
+                        data[j2 + step] = temp;
+                        print("第" + j + "次：", data);
+                    }
+                }
+            }
+        }
     }
 
     public static void print(String profix, int[] data) {
